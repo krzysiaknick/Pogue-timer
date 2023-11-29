bbfc8abbead198e00e172fb2f6f153989f133833
Nick Krzysiak <krzysiaknick@gmail.com>
2023-11-29 12:14:03 -0600

    switched-to-a-dropdown-configuration

---
+++ pougue-timer.js
@@ -0,0 +1,65 @@
+let minute_input = "";
+let time = minute_input * 60;
+let countdownInterval;
+let showSeconds = true;
+
+function start() {
+    // Check if countdown is already running
+    if (countdownInterval) {
+        return;
+    }
+
+    countdownInterval = setInterval(countdown, 1000);
+}
+
+function pause() {
+    clearInterval(countdownInterval);
+    countdownInterval = null;
+}
+
+
+
+function countdown() {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    updateDisplay(minutes, seconds);
+    time--;
+
+    if (time < 0) {
+        clearInterval(countdownInterval);
+        countdownInterval = null;
+        alert('Countdown finished!');
+    }
+}
+
+function toggleDisplay() {
+    showSeconds = !showSeconds;
+    updateDisplay();
+}
+
+function updateDisplay(minutes, seconds) {
+    if (typeof minutes === 'undefined' && typeof seconds === 'undefined') {
+        // Display the initial message
+        document.getElementById('countdown').innerHTML = minute_input;
+    } else if (typeof seconds === 'undefined') {
+        // Display only minutes when seconds are not provided
+        const formattedMinutes = minutes < 10 ? String(minutes) : String(minutes).padStart(2, '0');
+        const display = showSeconds ? `${formattedMinutes}:00` : `${formattedMinutes}`;
+        document.getElementById('countdown').innerHTML = display;
+    } else {
+        // Display both minutes and seconds
+        const formattedMinutes = minutes < 10 ? String(minutes) : String(minutes).padStart(2, '0');
+        const display = showSeconds
+            ? `${formattedMinutes}:${String(seconds).padStart(2, '0')}`
+            : `${formattedMinutes}`;
+        document.getElementById('countdown').innerHTML = display;
+    }
+}
+
+
+
+function userinput(){
+    let timeselection = document.getElementById("time").selectedOptions[0].value;
+    time = timeselection * 60;
+    updateDisplay(timeselection, 0);
+}
